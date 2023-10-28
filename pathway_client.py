657dac9823d30c016f8130cb4fd37a0b1e3c28a1
Arush-Pimpalkar <arush.pimpalkar@gmail.com>
2023-10-29 01:12:50 +0530

    fixed_pathway

--- pathway_client.py
+++ pathway_client.py
@@ -6,7 +6,8 @@ import os
 data_input = input(">> ")
 data = {
     "user": "user",
-    "query": data_input + ",the command should be the last word(s) of the sentence",
+    "query": data_input
+    + ",the command should be the last word(s) of the single sentence and in double quotes",
 }
 
 post_response = requests.post("http://localhost:8080/", json=data)
@@ -33,14 +34,14 @@ m3 = re.search(pattern3, text)
 if m1:
     found = m1.group(2)
     print("Pattern 1 match: ", found)
- #   os.system(found)
+#  os.system(found)
 elif m2:
     found = m2.group(2)
     print("Pattern 2 match: ", found)
- #   os.system(found)
+#  os.system(found)
 elif m3:
     found = m3.group(2)
     print("Pattern 3 match: ", found)
- #   os.system(found)
+#  os.system(found)
 else:
     print("No match found.")
